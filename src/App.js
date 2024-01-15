91574e90c7d720e3aca9bb72412a5e0de586e4cd
Kunal Kasera <kunalkasera456@gmail.com>
2024-01-15 17:51:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Componenets/Navbar";
+import About  from "./Componenets/About";
+import Section from "./Componenets/Section";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import sectionData from "./Componenets/data/sectionData";
+
+const App = ()=> {
+
+
+    return(
+        <div>
+            <Navbar/>
+            <About/>
+            {
+                sectionData.map((item,index)=>(
+                    <Section key={index}
+                        title={item.title}
+                        description={item.description}
+                    />
+                ))
+            }
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Componenets/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return(
+        <nav className="navbar">
+            <h1>Kunal Kumar</h1>
+            <div className="menu">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+                <a href="#">Projects</a>
+                <a href="#">Qualifications</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
